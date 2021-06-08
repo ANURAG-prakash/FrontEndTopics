faeebe3b3dc2853d600c591ea375a407a71cd6da
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-06-08 05:47:29 +0530

    Render props is done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-import ComponentsC from './components/ComponentsC'
-import { UserProvider } from './components/userContext';
 import PortalDemo from './components/PortalDemo'
+import ComponentsC from './components/ComponentsC'
 import ClickCounter from './components/ClickCounter'
- import HoverCounter from './components/HoverCounter'
- 
+import HoverCounter from './components/HoverCounter'
+import ClickCounterTwo from './components/ClickCounterTwo'
+import HoverCounterTwo from './components/HoverCounterTwo'
+import User from './components/User'
+import Counter from './components/Counter'
+import { UserProvider } from './components/userContext';
+
 function App() {
   return (
     <div className="App">
-      <UserProvider value="Anurag" >
+      {/* <UserProvider value="Anurag" >
       <ComponentsC />
-      </UserProvider>
-       <PortalDemo/>
-         <ClickCounter/>
-     <HoverCounter/> 
-     
+      </UserProvider> */}
+      {/* <PortalDemo/> */}
+      {/* <ClickCounter />
+      <HoverCounter /> */}
+      {/* <ClickCounterTwo />
+      <HoverCounterTwo />
+      <User render={(isLoggedIn) => isLoggedIn ? 'Anurag' : 'Guest'} /> */}
+       {/* <Counter
+        render={(count, incrementCount) => (
+          <ClickCounterTwo count={count} incrementCount={incrementCount}/>
+        )} />
+      <Counter
+        render={(count, incrementCount) => (
+          <HoverCounterTwo count={count} incrementCount={incrementCount}/>
+        )} />  */}
+
     </div>
   );
 }
