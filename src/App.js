cff3e1800aee639aacd6237321662431d93fc5a1
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-06-08 05:31:43 +0530

    HighOrder Component is done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import ComponentsC from './components/ComponentsC'
 import { UserProvider } from './components/userContext';
 import PortalDemo from './components/PortalDemo'
-
+import ClickCounter from './components/ClickCounter'
+ import HoverCounter from './components/HoverCounter'
+ 
 function App() {
   return (
     <div className="App">
@@ -11,6 +13,8 @@ function App() {
       <ComponentsC />
       </UserProvider>
        <PortalDemo/>
+         <ClickCounter/>
+     <HoverCounter/> 
      
     </div>
   );
