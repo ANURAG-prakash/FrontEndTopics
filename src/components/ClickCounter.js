5e2c8ba811e6c42d72a85cd11cb34d3533ce0d16
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-06-12 07:41:49 +0530

    profilers has been emplemented in clickcounter

--- src/components/ClickCounter.js
+++ src/components/ClickCounter.js
@@ -1,5 +1,10 @@
-import React, { Component } from 'react'
+import React,{Profiler,Component} from 'react'
 import UpdatedComponents from './withCounter'
+
+
+const logTimes = (id,phase,actualTime,baseTime,startTime,commitTime) =>{
+console.table({id,phase,actualTime,baseTime,startTime,commitTime})
+}
 
 class ClickCounter extends Component {
     
@@ -7,8 +12,11 @@ class ClickCounter extends Component {
     render() {
         const {count , incrementCount } =this.props
         return (
+            
             <div>
+                <Profiler id="table-info" onRender={logTimes}>
                 <button onClick={incrementCount}> Click {count} times</button>
+                </Profiler>
             </div>
         )
     }
